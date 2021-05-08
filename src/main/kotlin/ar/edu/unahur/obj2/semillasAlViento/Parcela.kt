ff2630cdf23270071d535e847217c6a3c40c0572
leozeballez <zeballezleonel@hotmail.com>
2021-05-08 02:33:02 -0300

    terminado.

--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Parcela.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Parcela.kt
@@ -2,25 +2,27 @@ package ar.edu.unahur.obj2.semillasAlViento
 
 class Parcela(val ancho: Int, val largo: Int, val horasSolPorDia: Int) {
   val plantas = mutableListOf<Planta>()
-  var cantidadPlantas = 0// un poco bastante redundante.. completamente innecesario, se podria obtener preguntandole el size a la lista de plantas.
+  var cantidadPlantas = 0 // un poco bastante redundante.. completamente innecesario, se podria obtener preguntandole el size a la lista de plantas. Redundancia Mínima.
 
   fun superficie() = ancho * largo
   fun cantidadMaximaPlantas() =
-    if (ancho > largo) ancho * largo / 5 else ancho * largo / 3 + largo //raro, esta medio feo.. revisar.
+    if (ancho > largo) ancho * largo / 5 else ancho * largo / 3 + largo //no se está utilizando el método superficie(). Redundancia Mínima.
 
   fun plantar(planta: Planta) {
     if (cantidadPlantas == this.cantidadMaximaPlantas()) {
-      println("Ya no hay lugar en esta parcela")
+      println("Ya no hay lugar en esta parcela") // esto está mal, reporta el error como un mensaje en consola. Afecta la Robustez, debe arrojar un error.
     } else if (horasSolPorDia > planta.horasDeSolQueTolera() + 2) {
-      println("No se puede plantar esto acá, se va a quemar")
+      println("No se puede plantar esto acá, se va a quemar") // esto está mal, reporta el error como un mensaje en consola. Afecta la Robustez, debe arrojar un error.
     } else {
       plantas.add(planta)
-      cantidadPlantas += 1
-    }// FALTA IMPLEMENTACION DE TIENE COMPLICACIONES.
+      cantidadPlantas += 1 // la cantidad de plantas dijimos que se puede consultar con el size. Redundancia Mínima.
+    }
   }
+
+  // FALTA IMPLEMENTACION DE TIENE COMPLICACIONES. Al estar hecha en la clase Planta, afecta la Cohesión.
 }
 
-class Agricultora(val parcelas: MutableList<Parcela>) {
+class Agricultora(val parcelas: MutableList<Parcela> /*debería ser inmutable la lista, ya que no se pueden comprar ni vender*/) {
   var ahorrosEnPesos = 20000// robustez: las tierras no se pueden comprar, ni la agricultora tiene ahorros. genera informacion erratica.
 
   // Suponemos que una parcela vale 5000 pesos
@@ -29,9 +31,9 @@ class Agricultora(val parcelas: MutableList<Parcela>) {
       parcelas.add(parcela)
       ahorrosEnPesos -= 5000
     }
-  }
+  } //Simplicidad. No se requiere en ningún momento esta función y se aclara que para este ejercicio no se venden ni se compran parcelas.
 
-  fun parcelasSemilleras() =// diuh, asquito.
+  fun parcelasSemilleras() =// diuh, asquito. (Des)Acoplamiento. El calculo esSemillera() debería estar en la Parcela.
     parcelas.filter {
       parcela -> parcela.plantas.all {
         planta -> planta.daSemillas()
@@ -39,7 +41,7 @@ class Agricultora(val parcelas: MutableList<Parcela>) {
     }
 
   fun plantarEstrategicamente(planta: Planta) {
-    val laElegida = parcelas.maxBy { it.cantidadMaximaPlantas() - it.cantidadPlantas }!!//falta que la parcela cumpla las condiciones de la planta. SIMPLICIDAD:el nombre laElegida es horrible, no se sabe de que habla.
-    laElegida.plantas.add(planta)//esto estaria bueno implementarlo aparte, parcelaEsIdealPara(planta)
+    val laElegida = parcelas.maxBy { it.cantidadMaximaPlantas() - it.cantidadPlantas }!!//falta que la parcela cumpla las condiciones de la planta. Cohesión: ésto de buscar la parcela debería estar en otro método.
+    laElegida.plantas.add(planta)//acá no está utilizando el método plantar(planta) que hace lo mismo, pero con las restricciones de la parcela. Acomplamiento.
   }
 }
--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
@@ -5,9 +5,10 @@ abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) {// muta
 
   fun parcelaTieneComplicaciones(parcela: Parcela) =
     parcela.plantas.any { it.horasDeSolQueTolera() < parcela.horasSolPorDia }
+  // este metodo debería estar en la clase de Parcela, no es responsabilidad de la Planta. Cohesión.
 
   abstract fun horasDeSolQueTolera(): Int
-  abstract fun daSemillas(): Boolean//el es fuerte es condicion para todas, por lo tanto se podria poner aca y usar un super en la sub con la otra condición.
+  abstract fun daSemillas(): Boolean//el es fuerte es condicion para todas, por lo tanto se podria poner aca y usar un super en la sub con la otra condición. -> Redundancia Mínima
 }
 
 class Menta(anioObtencionSemilla: Int, altura: Float) : Planta(anioObtencionSemilla, altura) {
@@ -15,7 +16,7 @@ class Menta(anioObtencionSemilla: Int, altura: Float) : Planta(anioObtencionSemi
   override fun daSemillas() = this.esFuerte() || altura > 0.4
 }
 
-class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean) : Planta(anioObtencionSemilla, altura) {
+class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean /* la SojaTransgenica debería ser otra clase que sea del tipo Soja.*/ ) : Planta(anioObtencionSemilla, altura) {
   override fun horasDeSolQueTolera(): Int  {
     // ¡Magia de Kotlin! El `when` es como un `if` pero más poderoso:
     // evalúa cada línea en orden y devuelve lo que está después de la flecha.
